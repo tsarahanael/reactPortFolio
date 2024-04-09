22c4712ff51332c7da5dd4f5d06376d1d10ff0e8
tsarahanael <Tsarahanael@gmail.com>
2024-04-09 13:27:58 +0100

    fix1

--- src/pages/NavbarApp.jsx
+++ src/pages/NavbarApp.jsx
@@ -9,7 +9,7 @@ function NavbarApp() {
       <Navbar.Brand>
         <Link to="/">
           <img
-            src="../public/images/logo.png"
+            src="images/logo.png"
             className="mr-3 h-6 sm:h-9"
             alt="Sample website logo"
           />
